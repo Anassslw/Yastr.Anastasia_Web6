10a520b92897d665311dbfa7d672aba9586a3d4b
Anassslw <91026718+Anassslw@users.noreply.github.com>
2022-06-03 02:30:19 +0700

    6

---
+++ Yastr.Anastasia_Web6/Yastr.Anastasia_Web6/Models/ApplicationRoles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yastr.Anastasia_Web6.Models
+{
+    public static class ApplicationRoles
+    {
+        public const String Administrators = nameof(ApplicationRoles.Administrators);
+    }
+}
---
+++ Yastr.Anastasia_Web6/Yastr.Anastasia_Web6/Services/IUserPermissionsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Yastr.Anastasia_Web6.Models;
+
+namespace Yastr.Anastasia_Web6.Services
+{
+    public interface IUserPermissionsService
+    {
+        Boolean CanEditPost(Post post);
+
+        Boolean CanEditPostComment(PostComment postComment);
+    }
+}
